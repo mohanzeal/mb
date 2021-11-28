d8a696ab1a3e838aec0f7a99ea8488e1c782be28
Mohan Windows <mohan.windows@gmail.com>
2021-11-28 17:55:59 +0530

    fix: tsc command failing with module not found error

--- packages/http-client/src/users/users.ts
+++ packages/http-client/src/users/users.ts
@@ -1,18 +1,8 @@
-import { AxiosResponse } from 'axios'
+import axios from 'axios'
 import AxiosHttpClient from '../http/AxiosHttpClient'
 
 export default class Users extends AxiosHttpClient {
-  async getUsers(): Promise<AxiosResponse<any>> {
-    try {
-      const response = await this.axiosClient.get('/')
-
-      if (response.ok) {
-        return response.data
-      } else {
-        console.log('getUsers request failed', response)
-      }
-    } catch (error: unknown) {
-      console.log(error)
-    }
+  getUsers(): boolean {
+    return true
   }
 }
