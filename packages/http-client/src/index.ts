8169e945dc85470808c345c98d05bcdbc9caa7e6
Mohan Windows <mohan.windows@gmail.com>
2021-11-28 21:33:04 +0530

    feat: link react-app and http-client

--- packages/http-client/src/index.ts
+++ packages/http-client/src/index.ts
@@ -1,7 +1,5 @@
-import AxiosHttpClient from './http/AxiosHttpClient'
-import Users from './users/users'
-
 export default {
-  axiosHttpClient: AxiosHttpClient.axiosInstance(),
-  users: new Users(),
+  users: {
+    name: 'John',
+  },
 }
