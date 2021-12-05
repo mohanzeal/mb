12132666a7132419ed287a05f88937d314a0ec97
Mohan Babu Bane <mohanbabu.bane@factset.com>
2021-12-05 22:30:24 +0530

    check build dir in next.config.js and some minor changes

--- packages/next-app/pages/index.tsx
+++ packages/next-app/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -14,7 +13,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!!</a>
         </h1>
 
         <p className={styles.description}>
@@ -61,7 +60,6 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
